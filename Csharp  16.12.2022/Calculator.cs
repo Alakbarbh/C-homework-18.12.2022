12691eb37b6bb3f06be26912f7f63522b31fa006
Alakbar Hasanov <alakbarbh@code.edu.az>
2022-12-18 17:31:51 +0400

    c#homework18.12.2022

---
+++ Csharp  16.12.2022/Calculator.cs
@@ -0,0 +1,56 @@
+﻿#region Task2
+public class Calculator
+{
+    public void Calculation()
+    {
+        float result = 0;
+
+
+        Console.WriteLine("Enter first number");
+        float num1 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter second number");
+        float num2 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter operator");
+        string op = Console.ReadLine();
+
+        switch (op)
+        {
+
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                if (num2 == 0)
+                {
+                    Console.WriteLine("Sifira bolunmur");
+                    break;
+                }
+                else
+                {
+                    result = num1 / num2;
+                    break;
+                }
+
+
+
+        }
+        Console.WriteLine("Result = " + result);
+
+
+    }
+}
+#endregion
+
+
+
+
+
+
